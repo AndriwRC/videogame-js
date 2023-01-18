93458b79598f3724314cfd15b8a1da8237df24a1
AndriwRC <randriw4@gmail.com>
2023-01-17 22:43:52 -0500

    refactor main algorithm in two functions

--- game.js
+++ game.js
@@ -1,12 +1,28 @@
 const canvas = document.querySelector('#game');
 const game = canvas.getContext('2d');
 
-window.addEventListener('load', startGame);
+let canvasSize;
+const canvasMinSize = 288;
+let elementSize;
+
+window.addEventListener('load', setCanvasSize);
+window.addEventListener('resize', setCanvasSize);
 
 function startGame() {
-  let canvasSize = window.innerWidth * 0.75;
-  const canvasMinSize = 288;
-  // const canvasMaxSize = 500;
+  elementSize = (Math.floor(canvasSize) / 10) - 1;
+
+  game.font = elementSize + 'px Verdana';
+  game.textBaseline = 'top';
+
+  for (let i = 0; i < 10; i++) {
+    for (let j = 0; j < 10; j++) {
+      game.fillText(emojis['X'], (elementSize * i), (elementSize * j) + 5);
+    }
+  }
+}
+
+function setCanvasSize() {
+  canvasSize = window.innerWidth * 0.75;
 
   if (canvasSize < canvasMinSize) {
     canvasSize = canvasMinSize;
@@ -18,25 +34,5 @@ function startGame() {
   canvas.setAttribute('width', canvasSize);
   canvas.setAttribute('height', canvasSize);
 
-  // const elementSize = (canvasSize / 10) - 1;
-  const elementSize = (Math.floor(canvasSize) / 10) - 1;
-  
-  game.font = elementSize + 'px Verdana';
-  game.textBaseline = 'top';
-  // game.fillText(emojis['X'], canvasSize - elementSize, 0);
-  
-  for (let i = 0; i < 10; i++) {
-    for (let j = 0; j < 10; j++) {
-      game.fillText(emojis['X'], (elementSize * i), (elementSize * j) + 5);
-    }
-  }
-  // game.fillText("Platzi", -5, -2);
-
-  console.log({canvasSize, elementSize});
-
-  // game.fillRect(0,0,100,100);
-  // game.clearRect(25,25,75,50);
-  // game.font = '20px Verdana';
-  // game.textBaseline = 'top';
-  // game.fillText('Platzi', 0, Math.floor(canvasSize));
+  startGame();
 }
