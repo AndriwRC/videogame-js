8eb675e03610fdd9924a2646723d8a3dc7e72154
AndriwRC <randriw4@gmail.com>
2023-01-26 00:35:13 -0500

    add two more maps

--- maps.js
+++ maps.js
@@ -46,3 +46,27 @@ maps.push(`
   XX-----OXX
   XXXXXXXXXX
 `);
+maps.push(`
+  O--X---XXX
+  XX-X-X-XXX
+  XX---X-XXX
+  XX--X--XXX
+  X--X--XXXX
+  --XX-XXXXI
+  -X--------
+  -XXXXX-XXX
+  -------XXX
+  XXXXXXXXXX
+`);
+maps.push(`
+  XXXXXXXXXX
+  X---------
+  X-XXXXXXX-
+  X-X-----X-
+  X-X-XXX-X-
+  X-X-XIX-XO
+  X-X---X-X-
+  X-XXXXX-X-
+  X-------X-
+  XXXXXXXX--
+`);
